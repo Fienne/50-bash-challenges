c55080d1c93c96907f3274b5f241de38b1c88b24
Elroy Kanye <elroykanye@gmail.com>
2021-07-05 21:13:10 +0100

    function description added...minimal tho

--- exercise15.sh
+++ exercise15.sh
@@ -1,3 +1,7 @@
+# Author: Elroy Kanye
+# This function prints the nth fibonnaci number of a number using recursion to evaluate it.
+# The first argument is the value of n
+# The function uses two extra arguments for previous and current values for the fibonacci number
 nfibonnaci () {
     n=$1
 
